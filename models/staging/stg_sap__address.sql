2f1a1aefb9a63fb302e7ab08c1529420b9192f9d
Lorenzo Sfredo <lorenzo.sfredo@indicium.tech>
2024-08-05 15:33:55 +0000

    Adicionando a primeira build das stagings, todas estão sujeitas a alterações.

---
+++ models/staging/stg_sap__address.sql
@@ -0,0 +1,10 @@
+with
+    source_address as (
+        select
+            cast(addressid as int) as address_id,            
+            cast(stateprovinceid as int) as stateprovince_id,
+            address.city as city
+        from {{ source('source', 'address') }}
+    )
+select *
+from source_address
---
+++ models/staging/stg_sap__salesorderheader.sql
@@ -0,0 +1,20 @@
+with
+    source_salesorderheader as (
+        select
+            cast(salesorderid as int) as order_id
+            , cast(customerid as int) as customer_id
+            , cast(territoryid as int) as territory_id
+            , cast(creditcardid as int) as creditcard_id
+            , cast(shiptoaddressid as int) as shiptoaddress_id
+            , date(orderdate) as order_date
+            , date(duedate) as due_date
+            , date(shipdate) as ship_date
+            , cast(status as int) as status
+            , cast(subtotal as numeric) as subtotal
+            , cast(taxamt as numeric) as tax
+            , cast(freight as numeric) as freight
+            , cast(totaldue as numeric) as total
+    from {{ source('source', 'salesorderheader') }}
+    )
+select *
+from source_salesorderheader
